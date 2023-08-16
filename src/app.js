15554bb0f453ae9c546cbb7358c9a98699766713
Guy David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-08-16 19:20:19 +0000

    Completed Domain Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function log(input) {
+  console.log(input);
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const pronoun = ["my", "your", "his", "her", "our", "their"];
+const adj = ["awesome", "amazing", "wonderful", "beautiful", "fantastic", "splendid", "maravellous", "incredible"];
+const noun = ["website", "business", "webpage", "idea", "blog", "onlineshop", "mafia-ring"]; 
+const extensions = [".com", ".co.uk", ".es", ".org", ".net"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for(let partOne of pronoun) {
+  let currentPronoun = partOne; 
+  
+  for(let partTwo of adj) {
+    let currentAdj = partTwo;
+
+    for(let partThree of noun) {
+      let currentNoun = partThree;
+    
+      for(let partFour of extensions) {
+        let currentExt = partFour;
+
+        let domain = `${currentPronoun}${currentAdj}${currentNoun}${currentExt}`;
+        log(domain);
+      }
+    }
+  }
+} 
+
